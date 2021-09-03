0e959ec5a621bcd5263b210b83d70af35842850c
Ashif-coder <ashif8520@gmail.com>
2021-09-03 16:29:18 +0530

    Login page done

--- lib/home.dart
+++ lib/home.dart
@@ -5,30 +5,8 @@ Home({this.username});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
-
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/background.jpg"),
-                  fit: BoxFit.cover,
-                ),
-              ), /* add child content here */
-            ),
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/geany.png"),
-
-                ),
-              ), /* add child content here */
-            ),
-          ],
-        )
-      ),
+    return Scaffold(
+      backgroundColor: Colors.teal,
     );
   }
 }
